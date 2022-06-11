f2798a83b058b92430d1ec1d73207f08a998565e
Sanya2545 <sasha2003bug@gmail.com>
2022-06-11 21:38:01 +0300

    first commit, first part of class Box, has been added

---
+++ Hello_Cons_Dr_Methods/Box.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Cons_Dr_Methods
+{
+    class Box
+    {
+        public int StartX{ get; set; }
+        public int StartY{ get; set; }
+        public int Width{ get; set; }
+        public int Height{ get; set; }
+        public char SymbolBorder {
+            get
+            {
+               return SymbolBorder;
+            }
+            set
+            {
+                if(value == '*' || value == '+' || value == '.')
+                {
+                    SymbolBorder = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Wrong symbol to print border !");
+                }
+            }
+         }
+        public string Message { get; set; }
+
+        //1.  Implement public  auto-implement properties for start position (point position)
+        //auto-implement properties for width and height of the box
+        //and auto-implement properties for a symbol of a given set of valid characters (*, + ,.) to be used for the border 
+        //and a message inside the box
+
+        //2.  Implement public Draw() method
+        //to define start position, width and height, symbol, message  according to properties
+        //Use Math.Min() and Math.Max() methods
+        //Use draw() to draw the box with message
+
+        //3.  Implement private method draw() with parameters 
+        //for start position, width and height, symbol, message
+        //Change the message in the method to return the Box square
+        //Use Console.SetCursorPosition() method
+        //Trim the message if necessary
+
+    }
+}
---
+++ Hello_Cons_Dr_Methods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Cons_Dr_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box();
+            List<int> array = box.returnArray();
+            int MaxElement = box.MaxValue(array);
+            Console.WriteLine("Max element : " + MaxElement);
+            //try
+            //{
+            //    //Implement start position, width and height, symbol, message input
+               
+            //    //Create Box class instance
+
+            //    //Use  Box.Draw() method
+
+            //    Console.WriteLine("Press any key...");
+            //Console.ReadLine();
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Error!");
+            //}
+            
+        }
+    }
+}
